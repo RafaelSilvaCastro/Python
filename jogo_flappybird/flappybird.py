54a6aa33148b218b4d1ece5ab020571179debaca
Rafael da Silva Castro <rafael.silcastro.a@gmail.com>
2024-04-30 12:19:51 -0300

    criado o chao

--- jogo_flappybird/flappybird.py
+++ jogo_flappybird/flappybird.py
@@ -133,13 +133,28 @@ class Cano:
         if base_ponto or topo_ponto:
             return True
         else:
-            return False
-
+            return False    
         
         
-        
-
-
 class Chao:
-    pass
+    VELOCIDADE = 5
+    LARGURA = IMAGEM_CANO.get_width()
+    IMAGEM = IMAGEM_CANO
+    
+    def __init__(self, y):
+        self.y = y 
+        self.x1 = 0
+        self.x2 = self.LARGURA
+        
+    def mover(self):
+        self.x1 -= self.VELOCIDADE
+        self.x2 -= self.VELOCIDADE
+        
+        if self.x1 + self.LARGURA < 0:
+            self.x1 += self.LARGURA
+        if self.x2 + self.LARGURA < 0:
+            self.x2 +=  self.LARGURA
 
+    def desenhar(self, tela):
+        tela.blit(self.IMAGEM, (self.x1, self.y))
+        tela.blit(self.IMAGEM, (self.x2, self.y))
