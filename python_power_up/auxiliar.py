fbc076102697af1b8c28dd132d8ba0b27219781a
Rafael da Silva Castro <rafael.silcastro.a@gmail.com>
2024-05-08 10:34:15 -0300

    .

---
+++ python_power_up/auxiliar.py
@@ -0,0 +1,10 @@
+# Achar o ponto x e y do computador para ser clicado
+
+import pyautogui
+import time
+
+time.sleep(5)
+print(pyautogui.position())
+
+# saber o nome das teclas
+print(pyautogui.KEYBOARD_KEYS)
--- python_power_up/codigo.py
+++ python_power_up/codigo.py
@@ -6,6 +6,10 @@
     
 # para instalar: pip install pyautogui
 import pyautogui
+import time
+
+# tempo de delay para executar as tarefas
+pyautogui.PAUSE = 1
 
 # pyautogui.click -> clicar com o mouse
 # pyautogui.write -> escrever um texto
@@ -14,10 +18,33 @@ import pyautogui
 
 # abir o navegador (chrome)
 pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
 
 # entar no site
-    
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+
+# aqui pode ser que ele demore alguns segundo pra carregar o site 
+time.sleep(3)
+
 # 2. Fazer login
+pyautogui.click(x=1162, y=437) # achar a posição da caixa de texto do email
+pyautogui.write("rafael.castro@gmail.com")
+
+pyautogui.press("tab") # passou para o campo de senha
+pyautogui.write("12345")
+
+pyautogui.press("tab") # passou para o botao de logar
+pyautogui.press("enter")
+
+time.sleep(3) # garantia para que a proxima pagina abre 
+
 # 3. Abir/Importar a base de dados de produtos para cadastrar 
+# pip install pandas numpy openpyxl
+import pandas
+
+
+
 # 4. Cadastrar um produto
 # 5. Repetir isso tudo ate acabar a lista de produtos
