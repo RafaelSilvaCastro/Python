13bdd7ba18db8eaf51d067a09aef9adbc4273f30
Rafael da Silva Castro <rafael.silcastro.a@gmail.com>
2024-05-08 09:36:01 -0300

    começando a automação

---
+++ python_power_up/codigo.py
@@ -0,0 +1,23 @@
+# Passo a passo do projeto
+
+# 1. Abrir o sistema da empresa
+    # https://dlp.hashtagtreinamentos.com/python/intensivao/login
+    
+    
+# para instalar: pip install pyautogui
+import pyautogui
+
+# pyautogui.click -> clicar com o mouse
+# pyautogui.write -> escrever um texto
+# pyautogui.press -> precionar uma tecla do teclado
+# pyautogui.hotkey -> apertar um conjunto de teclas (ctrl c, ctrl v, alt tab)
+
+# abir o navegador (chrome)
+pyautogui.press("win")
+
+# entar no site
+    
+# 2. Fazer login
+# 3. Abir/Importar a base de dados de produtos para cadastrar 
+# 4. Cadastrar um produto
+# 5. Repetir isso tudo ate acabar a lista de produtos
