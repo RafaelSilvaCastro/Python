5b3d0967562a8cfaf7472baa19ca61f9909aec21
Rafael da Silva Castro <rafael.silcastro.a@gmail.com>
2024-05-10 09:42:06 -0300

    começando python web

---
+++ python_hashtag/python_dev_flet/hashzap.py
@@ -0,0 +1,25 @@
+# Titulo: Hashzap
+# Botao de iniciar
+    # popup (janela na frente da tela)
+    # Titulo: Bem vndo ao Hashzap 
+    #campo de texto -> escreva seu nome no chat
+    # botao: entrar no chat
+        # sumir com o titulo hashzap 
+        # fechar a janela (popup)
+        # carregar o chat
+            # as mensagens que ja foram enviadas (chat)
+            # campo: digite sua mensagem 
+            # botao de enviar
+            
+# Flet e usado para frontend e backend no python  
+# pip install flet         
+
+# importar o flet
+import flet as ft 
+
+# função principal do seu aplicativo
+def main(pagina):
+    # criar todas as funcionalidades
+    
+# rodas o aplicativo
+ft.app(main)
