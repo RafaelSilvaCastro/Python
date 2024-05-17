2fd66d9ea7f672dd6cda6f2ece88c5484c2b4b5d
Rafael da Silva Castro <rafael.silcastro.a@gmail.com>
2024-05-17 17:55:37 -0300

    alterando

--- python_dio/Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
+++ python_dio/Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
@@ -1,8 +1,14 @@
-pessoa = {"nome": "Guilherme", "idade": 28}
+pessoa = {
+    "nome": "Guilherme", 
+    "idade": 28
+    }
 print(pessoa)
 
 pessoa = dict(nome="Guilherme", idade=28)
 print(pessoa)
 
 pessoa["telefone"] = "3333-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
+print(pessoa)
+
+pessoa["nome"] = "Rafael"
 print(pessoa)
