176f49d8647e5227e5ed6282732635445cb5125e
Rafael da Silva Castro <rafael.silcastro.a@gmail.com>
2024-05-22 15:36:58 -0300

    .

--- python_dio/Programação_Orientada a Objetos/02_Classes_e_Objetos/01_desafio_bicicletaria.py
+++ python_dio/Programação_Orientada a Objetos/02_Classes_e_Objetos/01_desafio_bicicletaria.py
@@ -28,7 +28,6 @@ class Bicicleta:
     def __str__(self):
         return f"{self.__class__.__name__}: {', '.join([f'{chave} = {valor}' for chave, valor in self.__dict__.items()])}"
         
-        
 # b1 = Bicicleta("azul", "caloi", 2022, 600)
 # b1.buzinar()
 # b1.correr()
