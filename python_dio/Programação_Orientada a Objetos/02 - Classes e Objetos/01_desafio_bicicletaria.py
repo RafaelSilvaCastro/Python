5012b2d7fd85cf4bedbd5237168eda118efc8f2b
Rafael da Silva Castro <rafael.silcastro.a@gmail.com>
2024-05-22 15:29:45 -0300

    arrumando nome das pastas

--- python_dio/Programação_Orientada a Objetos/02 - Classes e Objetos/01_desafio_bicicletaria.py
+++ python_dio/Programação_Orientada a Objetos/02 - Classes e Objetos/01_desafio_bicicletaria.py
@@ -1,30 +1,44 @@
+# criando uma classe
 class Bicicleta:
     def __init__(self, cor, modelo, ano, valor):
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
-
+        
+    # metodos  
     def buzinar(self):
-        print("Plim plim...")
-
+        print("Plim plim..")
+        
     def parar(self):
-        print("Parando bicicleta...")
+        print("Parando bicicleta ...")
         print("Bicicleta parada!")
-
+        
     def correr(self):
-        print("Vrummmmm...")
-
+        print("Vruuummm ...")
+        
+    # def get_cor(self):
+    #    return self.cor
+        
+    # um jeito melhor de mostrar
+    # def __str__(self):
+    #     return f"Bicicleta: cor = {self.cor}, modelo = {self.modelo}, ano = {self.ano}, valor = {self.valor}"
+    
+    # jeito altomatico de mostrar
     def __str__(self):
-        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
-
-
-b1 = Bicicleta("vermelha", "caloi", 2022, 600)
-b1.buzinar()
-b1.correr()
-b1.parar()
-print(b1.cor, b1.modelo, b1.ano, b1.valor)
+        return f"{self.__class__.__name__}: {', '.join([f'{chave} = {valor}' for chave, valor in self.__dict__.items()])}"
+        
+        
+# b1 = Bicicleta("azul", "caloi", 2022, 600)
+# b1.buzinar()
+# b1.correr()
+# b1.parar()
+# print(b1.cor, b1.modelo, b1.ano, b1.valor)
 
 b2 = Bicicleta("verde", "monark", 2000, 189)
 print(b2)
-b2.correr()
+# b2.buzinar() # Bicicleta.buzinar(b2)
+
+# print(b2.get_cor())
+  
+    
