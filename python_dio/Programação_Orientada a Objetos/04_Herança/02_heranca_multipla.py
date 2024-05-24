07cd1a9efa03ef83bc18330de0ec90fb68b87f64
Rafael da Silva Castro <rafael.silcastro.a@gmail.com>
2024-05-24 15:48:06 -0300

    Update 02_heranca_multipla.py

--- python_dio/Programação_Orientada a Objetos/04_Herança/02_heranca_multipla.py
+++ python_dio/Programação_Orientada a Objetos/04_Herança/02_heranca_multipla.py
@@ -1,34 +1,35 @@
 class Animal:
     def __init__(self, nro_patas):
         self.nro_patas = nro_patas
-
-    def __str__(self):
-        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
-
+        
 
 class Mamifero(Animal):
-    def __init__(self, cor_pelo, **kw):
-        self.cor_pelo = cor_pelo
-        super().__init__(**kw)
-
+    def __init__(self, nro_patas):
+        self.nro_patas = nro_patas
+        super().__init__(nro_patas)
 
 class Ave(Animal):
-    def __init__(self, cor_bico, **kw):
-        self.cor_bico = cor_bico
-        super().__init__(**kw)
+    def __init__(self, nro_patas):
+        self.nro_patas = nro_patas
+        super().__init__(nro_patas)
 
 
 class Gato(Mamifero):
     pass
 
 
-class Ornitorrinco(Mamifero, Ave):
-    def __init__(self, cor_bico, cor_pelo, nro_patas):
-        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
+class Ornintorico(Mamifero, Ave):
+    pass
 
 
-gato = Gato(nro_patas=4, cor_pelo="Preto")
-print(gato)
+class Cachorro(Mamifero):
+    pass
 
-ornitorrinco = Ornitorrinco(nro_patas=2, cor_pelo="vermelho", cor_bico="laranja")
-print(ornitorrinco)
+
+class Leao(Mamifero):
+    pass
+
+
+
+
+
