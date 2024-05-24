eac55ffcd173d85ac5cd13e0319b7007c9eb416d
Rafael da Silva Castro <rafael.silcastro.a@gmail.com>
2024-05-24 17:01:48 -0300

    Update 02_heranca_multipla.py

--- python_dio/Programação_Orientada a Objetos/04_Herança/02_heranca_multipla.py
+++ python_dio/Programação_Orientada a Objetos/04_Herança/02_heranca_multipla.py
@@ -2,24 +2,38 @@ class Animal:
     def __init__(self, nro_patas):
         self.nro_patas = nro_patas
         
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave} = {valor}' for chave, valor in self.__dict__.items()])}"
+        
 
 class Mamifero(Animal):
-    def __init__(self, nro_patas):
-        self.nro_patas = nro_patas
-        super().__init__(nro_patas)
+    def __init__(self, cor_pelo, **kw):
+        self.cor_pelo = cor_pelo
+        super().__init__(**kw)
 
 class Ave(Animal):
-    def __init__(self, nro_patas):
-        self.nro_patas = nro_patas
-        super().__init__(nro_patas)
+    def __init__(self, cor_bico, **kw):
+        self.cor_bico = cor_bico
+        super().__init__(**kw)
 
 
 class Gato(Mamifero):
     pass
 
 
-class Ornintorico(Mamifero, Ave):
-    pass
+class FalarMixin:
+    def falar(self):
+        return "oi estou falando"
+    
+class Ornintorrinco(Mamifero, Ave, FalarMixin):
+    def __init__(self, cor_bico, cor_pelo, nro_patas):
+        #  print(ornintorinco)
+        #  print(ornintorinco.mro())
+        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
+        
+    # def __str__(self):
+    #     return "Ornitorrinco"   
+    
 
 
 class Cachorro(Mamifero):
@@ -30,6 +44,11 @@ class Leao(Mamifero):
     pass
 
 
+gato = Gato(nro_patas=4, cor_pelo="preto")
+print(gato)
 
+ornintorinco = Ornintorrinco(nro_patas=2, cor_pelo="marrom", cor_bico="laranja")
+print(ornintorinco)
+print(ornintorinco.falar())
 
 
