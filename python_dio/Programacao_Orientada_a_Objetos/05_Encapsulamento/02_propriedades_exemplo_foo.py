2878dce91d2af2948f3836ececea4810dcc54e34
Rafael da Silva Castro <rafael.silcastro.a@gmail.com>
2024-05-27 12:01:45 -0300

    .

--- python_dio/Programacao_Orientada_a_Objetos/05_Encapsulamento/02_propriedades_exemplo_foo.py
+++ python_dio/Programacao_Orientada_a_Objetos/05_Encapsulamento/02_propriedades_exemplo_foo.py
@@ -1,19 +1,18 @@
 class Foo:
     def __init__(self, x=None):
         self._x = x
-
-    @property
+        
+    @property    
     def x(self):
         return self._x or 0
-
+    
     @x.setter
     def x(self, value):
         self._x += value
-
+    
     @x.deleter
     def x(self):
         self._x = 0
-
 
 foo = Foo(10)
 print(foo.x)
